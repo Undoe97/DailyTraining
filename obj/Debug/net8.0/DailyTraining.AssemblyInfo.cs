42cbe9e14508e3917474a09438abdb8f4b6dabb0
DoEonKim <rgnjkl1222@gmail.com>
2024-11-27 14:48:19 +0900

    5개 숫자 오름차순, 주사위 게임

--- obj/Debug/net8.0/DailyTraining.AssemblyInfo.cs
+++ obj/Debug/net8.0/DailyTraining.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DailyTraining")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c41b33d44a975befe0bc21f599eeeffe70276c88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b774271027798ff1e38bde7960c9c7a51a5c802")]
 [assembly: System.Reflection.AssemblyProductAttribute("DailyTraining")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DailyTraining")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
